86e328d223cbe470ee3808950af2ff07b156bebf
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-06 16:38:07 +0200

    Remove comments

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,11 @@
   interested in philosophy, psychology, mathematics, physics and technology.
   Currently studying for my bachelor's degree in Computer Science.
 
-<!--   <div class="section">
-    <h2>Personal Projects</h2>
-    <ul>
-      <li><a href="#">UNABOM Encrypter</a></li>
-      <li><a href="#">Random Dungeon Generator</a></li>
-      <li><a href="#">Tetris Multiplayer</a></li>
-      <li><a href="#">This Website</a></li>
-    </ul>
-  </div>
-
-  <div class="section">
-    <h2>School Projects</h2>
-    <ul>
-      <li><a href="#">Void</a></li>
-      <li><a href="#">Space Race</a></li>
-      <li><a href="#">Deep Sea Diver</a></li>
-      <li><a href="#">Nomad</a></li>
-      <li><a href="#">Gravity Surge</a></li>
-    </ul>
-  </div> -->
-
   <div class="section">
     <h2>Education</h2>
     <ul>
       <li><a href="/education/courses">Courses I've followed</a></li>
       <li><a href="/education/books">Books I've read</a></li>
-      <!--<li><a href="/education/cv">Curriculum vitae</a></li> -->
     </ul>
   </div>
 </template>
