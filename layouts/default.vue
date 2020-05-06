7320af365a599c4ec1a07058ee8f9803597a106a
Nicky van Urk <nickyvanurk@gmx.com>
2020-05-06 18:35:05 +0200

    Add social icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,8 @@
       <div class="container">
         <a href="/"><font-awesome-icon :icon="['fas', 'home']"/></a>
         <a href="/contact"><font-awesome-icon :icon="['far', 'envelope']"/></a>
+        <a href="https://github.com/nickyvanurk" target="_blank"><font-awesome-icon :icon="['fab', 'github']"/></a>
+        <a href="https://linkedin.com/in/nickyvanurk" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin']"/></a>
       </div>
     </nav>
 
