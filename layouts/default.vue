316c51805348224595de8028005c7ca925664205
Nicky van Urk <nickyvanurk@gmx.com>
2020-09-06 14:21:08 +0200

    Add basic nav styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,8 @@
         </a>
 
         <nav>
-          <a href="/">Home</a>
-          <a href="/blog">Blog</a>
-          <a href="/contact">Contact</a>
+          <a class="menu-item" href="/blog">Blog</a>
+          <a class="menu-item" href="/contact">Contact</a>
         </nav>
       </header>
 
@@ -19,10 +18,33 @@
 </template>
 
 <style>
+:root {
+  --brand-color: #ff572f;
+  --primary-text-color: #333;
+  --secondary-text-color: #999;
+  --white-color: #fff;
+  --lighter-gray-color: #f6f6f6;
+  --light-gray-color: #e6e6e6;
+  --mid-gray-color: #ccc;
+  --dark-gray-color: #444;
+  --darker-gray-color: #1a1a1a;
+  --black-color: #000;
+  --green-color: #28a745;
+  --orange-color: #ffc107;
+  --red-color: #dc3545;
+  --facebook-color: #3b5998;
+  --twitter-color: #1da1f2;
+  --rss-color: #f26522;
+  --animation-base: ease-in-out;
+  --font-base: Muli, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica,
+    Arial, sans-serif;
+  --navbar-height: 80px;
+  --scrollbar: 0px;
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
+  font-family: var(--font-base);
+  font-size: 62.5%; /* 10 px */
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
@@ -36,6 +58,20 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
+}
+
+body {
+  font-size: 1.5rem;
+  line-height: 1.6;
+}
+
+a {
+  color: var(--darker-gray-color);
+  text-decoration: none;
+}
+
+a:hover {
+  opacity: 0.8;
 }
 
 .container {
@@ -78,7 +114,7 @@ html {
 .site-header {
   display: flex;
   align-items: center;
-  height: 80px;
+  height: var(--navbar-height);
 }
 
 .site-header .logo img {
@@ -89,5 +125,10 @@ html {
 .site-header nav {
   margin-left: auto;
   margin-right: auto;
+}
+
+.menu-item {
+  font-weight: bold;
+  margin: 0 1.5rem;
 }
 </style>
