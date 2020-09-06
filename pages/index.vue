b7eeec7cdd9cf144b7bbc8a84df5230aad3dc271
Nicky van Urk <nickyvanurk@gmx.com>
2020-09-06 14:56:16 +0200

    Add cover text

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <div class="cover">
+      <div class="cover-content">
+        <div class="cover-description">
+          Hi there 👋 My name is <strong>Nicky van Urk</strong>, programmer
+          &amp; tech enthusiast. I'm currently studying for my bachelor's degree
+          in Computer Science.
+        </div>
+      </div>
+    </div>
+
     <h1>Blog Posts</h1>
     <ul>
       <li v-for="article of articles" :key="article.slug">
@@ -30,3 +40,22 @@ export default {
   },
 }
 </script>
+
+<style>
+.cover {
+  margin-top: 4.5rem;
+}
+
+.cover-content {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 500px;
+  text-align: center;
+}
+
+.cover-description {
+  font-size: 1.7rem;
+}
+</style>
