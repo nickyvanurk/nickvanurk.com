217e564ef432aeb883be622099e089a1cd221c3b
Nicky van Urk <nickyvanurk@gmx.com>
2022-01-05 19:16:49 +0100

    Add ninja sprite

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let app = new PIXI.Application({ width: 640, height: 360, resizeTo: window, transparent: true });
+
+const ninja = {};
+
+PIXI.settings.RESOLUTION = window.devicePixelRatio;
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+document.body.appendChild(app.view);
+
+PIXI.Loader.shared.add('ninja', 'assets/spritesheets/ninja.json').load(setup)
+
+function setup() {
+    let sheet = PIXI.Loader.shared.resources.ninja.spritesheet;
+
+    ninja.idle = new PIXI.AnimatedSprite(sheet.animations['idle']);
+    ninja.idle.anchor.set(0.5, 1)
+    ninja.idle.x = app.renderer.width/4
+    ninja.idle.y = app.renderer.height
+    ninja.idle.scale.set(3)
+    ninja.idle.animationSpeed = 0.1;
+    ninja.idle.play()
+
+    app.stage.addChild(ninja.idle);
+}
+
+window.onresize = () => {
+    ninja.idle.x = app.renderer.width/4
+    ninja.idle.y = app.renderer.height
+}
